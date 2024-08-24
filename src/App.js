79d64ba1b496fbabb10c9ed9e5b4aed08b9334ea
shobhitsingh-2503 <shobhitsingh25032003@gmail.com>
2024-08-24 20:01:25 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "./global.css";
 import { IoMdSearch } from "react-icons/io";
 import Upload from "./components/Upload";
+import ListItem from "./components/ListItem";
 
 function App() {
   return (
@@ -14,12 +15,30 @@ function App() {
         <input
           type="text"
           id="srchBar"
-          placeholder="Enter Name or uploader.... "
+          placeholder="Enter Name of file or uploader.... "
         />
         <IoMdSearch id="srch" />
       </div>
       <Upload />
-      <div id="container"></div>
+      <div id="filter">
+        <input type="month" id="month" />
+        <select id="dept">
+          <option value="ALL">ALL</option>
+          <option value="Indian Meteorological department (IMD)">
+            Indian Meteorological department (IMD)
+          </option>
+          <option value="National center for medium range weather forecasting (NCMRWF)">
+            National center for medium range weather forecasting (NCMRWF)
+          </option>
+          <option value="Indian Institute of Tropical Metereologoy (IITM) Pune">
+            Indian Institute of Tropical Metereologoy (IITM) Pune
+          </option>
+          <option value="Miscellaneous">Miscellaneous</option>
+        </select>
+      </div>
+      <div id="container">
+        <ListItem />
+      </div>
       <div id="footer">
         <pre>Made with 💖 © Team </pre>
       </div>
